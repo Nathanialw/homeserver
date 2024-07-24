859ef0a64de540992ddb3e646ef068cc41ba5e3a
nathanial <nathanial28@gmail.com>
2024-07-24 01:12:37 -0600

    updateed update file

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@ git checkout -- *
 git pull
 chmod a+x webserver/app/main
 chmod 777 webserver/db/homeserver.sqlite3
+chmod 777 webserver/db
 sudo service homeserver restart
 sudo service homeserver status
