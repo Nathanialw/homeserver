d178a67aa264e35ce2a9750e64c1d96723cd13dd
nathanial <nathanial28@gmail.com>
2024-07-23 23:10:36 -0600

    trying video playback

--- webserver/main.go
+++ webserver/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 	content "webserver/src/Content"
 	db "webserver/src/DB"
 	lanbooks "webserver/src/LANBooks"
@@ -27,20 +28,41 @@ func main() {
 	db.Init()
 	r := httprouter.New()
 
+	// r.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Printf("Incoming request: %s %s\n", r.Method, r.URL.Path)
+	// 	w.WriteHeader(http.StatusNoContent)
+	// })
+
+	r.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Serve static files from /mnt/media, strip the leading part of the URL path
+		if strings.HasPrefix(r.URL.Path, "/mnt/media/") {
+			http.StripPrefix("/mnt/media/", http.FileServer(http.Dir("/mnt/media"))).ServeHTTP(w, r)
+		} else {
+			// If the path does not match, call the notfound handler
+			// notfound(w, r, httprouter.Params{})
+			fs := http.FileServer(http.Dir("../../public/"))
+			http.StripPrefix("/", fileServerWith404(fs))
+		}
+	})
+
 	// Serve 404 page for non-existent files
-	fs := http.FileServer(http.Dir("../../public/"))
-	r.NotFound = http.StripPrefix("/", fileServerWith404(fs))
+	// fs := http.FileServer(http.Dir("../../public/"))
+	// r.NotFound = http.StripPrefix("/", fileServerWith404(fs))
 
 	// Serve static files from /mnt/external
-	vs := http.FileServer(http.Dir("/mnt/media/"))
-	http.Handle("/movies/", http.StripPrefix("/movies/", vs))
+	// vs := http.FileServer(http.Dir("/mnt/media/"))
+	// http.Handle("/media/", http.StripPrefix("/media/", vs))
+	// http.HandleFunc("/media/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Printf("REQUESTED FILE: %s\n", r.URL.Path)
+	// 	http.StripPrefix("/media/", http.FileServer(http.Dir("/mnt/media"))).ServeHTTP(w, r)
+	// })
 
 	r.GET("/", home)
 	r.GET("/books", lanbooks.Home)
 	r.GET("/docs", landocs.Home)
 	r.GET("/games", langames.Home)
 
-	r.GET("/moves", lanmovies.Home)
+	r.GET("/movies", lanmovies.Home)
 	r.GET("/movie", lanmovies.ShowMovie)
 	r.GET("/addmovie", lanmovies.AddMovie)
 	r.POST("/submitmovie", lanmovies.SubmitMovie)
